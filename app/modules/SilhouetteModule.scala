9c6374c7b32ea1cac2cd1615e79071948f9c3bae
Jordan Bethea <jordan.bethea@gmail.com>
2021-05-22 18:58:15 -0400

    Add initial version of ranked voting radio button helper

--- app/modules/SilhouetteModule.scala
+++ app/modules/SilhouetteModule.scala
@@ -20,7 +20,7 @@ import models.db.UsersAndAuthentication.{AuthTokenDAO, AuthTokenDAOImpl, OAuth1I
 import net.codingwell.scalaguice.ScalaModule
 import play.api.Configuration
 import utils.DefaultEnv
-import services.{AuthTokenService, AuthTokenServiceImpl, BallotService, BallotServiceImpl, SlateService, SlateServiceImpl, UserService, UserServiceImpl}
+import services.{AuthTokenService, AuthTokenServiceImpl, BallotService, BallotServiceImpl, ResultsService, ResultsServiceImpl, SlateService, SlateServiceImpl, UserService, UserServiceImpl}
 import net.ceedubs.ficus.Ficus._
 import net.ceedubs.ficus.readers.ValueReader
 import net.ceedubs.ficus.readers.ArbitraryTypeReader._
@@ -57,6 +57,7 @@ class SilhouetteModule extends AbstractModule with ScalaModule {
     bind[UserService].to[UserServiceImpl]
     bind[SlateService].to[SlateServiceImpl]
     bind[BallotService].to[BallotServiceImpl]
+    bind[ResultsService].to[ResultsServiceImpl]
     bind[EventBus].toInstance(EventBus())
     bind[FingerprintGenerator].toInstance(new DefaultFingerprintGenerator(false))
     bind[IDGenerator].toInstance(new SecureRandomIDGenerator())
